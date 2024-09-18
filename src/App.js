7a8bdbe9c1f74d8e41ef416c80cbf8e9e027a716
GBottarlini <bottarlini.99@outlook.com>
2024-09-18 10:51:37 -0300

    proyecto de practica

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
 
-function App() {
+import "./App.css";
+import searchIcon from "./search.svg";
+
+const API_URL = "http://www.omdbapi.com?apikey=76523fa6";
+
+const movie1 = {
+  Title: "The Amazing Spiderman 2 Webb Cut",
+  Year: "2021",
+  imdbID: "tt18351128",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BNzI0MmQyMzYtZDAzNi00ZWZiLWFjMTgtNzQwOTRjYTFlM2Y3XkEyXkFqcGc@._V1_SX300.jpg",
+};
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearcTerm] = useState('');
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearcTerm(e.target.value)}
+        />
+        <img src={searchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2> No movies found</h2>
+        </div>
+      )}
     </div>
   );
-}
-
+};
 export default App;
---
+++ src/MovieCard.jsx
@@ -0,0 +1,26 @@
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="movie">
+      <div>
+        <p>{movie.Year}</p>
+      </div>
+
+      <div>
+        <img
+          src={
+            movie.Poster !== "N/A"
+              ? movie.Poster
+              : "https://via.placerholder.com/400 "
+          }
+          alt={movie.Title}
+        />
+      </div>
+      <div>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
