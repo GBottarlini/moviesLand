118f5062bf4ae9a103b892ed2b58d6fee21e0695
GBottarlini <bottarlini.99@outlook.com>
2024-09-18 11:11:41 -0300

    Correcion de link API

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 import searchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com?apikey=76523fa6";
+const API_URL = "https://www.omdbapi.com?apikey=76523fa6";
 
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-  
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
