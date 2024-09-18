e4f05874dae90fac0859f537d83763e8dff65645
GBottarlini <bottarlini.99@outlook.com>
2024-09-18 11:01:33 -0300

    Correccion de error en archivo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
-
 import "./App.css";
 import searchIcon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com?apikey=76523fa6";
 
-const movie1 = {
-  Title: "The Amazing Spiderman 2 Webb Cut",
-  Year: "2021",
-  imdbID: "tt18351128",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BNzI0MmQyMzYtZDAzNi00ZWZiLWFjMTgtNzQwOTRjYTFlM2Y3XkEyXkFqcGc@._V1_SX300.jpg",
-};
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearcTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+  
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
@@ -36,7 +28,7 @@ const App = () => {
         <input
           placeholder="Search for movies"
           value={searchTerm}
-          onChange={(e) => setSearcTerm(e.target.value)}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <img src={searchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
       </div>
